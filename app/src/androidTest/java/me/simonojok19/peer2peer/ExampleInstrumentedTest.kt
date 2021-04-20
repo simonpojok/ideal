912e08acab2200b8662d4658a9dcc5d6d135a521
simonojok19 <simonojok19@gmail.com>
2021-04-20 23:20:41 +0300

    started the project

---
+++ app/src/androidTest/java/me/simonojok19/peer2peer/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package me.simonojok19.peer2peer
+
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.ext.junit.runners.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("me.simonojok19.peer2peer", appContext.packageName)
+    }
+}
