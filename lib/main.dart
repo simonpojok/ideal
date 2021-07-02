695de996c270b5e24635c59ca9145356e9d219e2
simonojok19 <simonojok19@gmail.com>
2021-07-02 21:03:08 +0300

    cleaned the search bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:peer2peer/screens/dashboard/dashboard_screen.dart';
 import './screens/login/login_screen.dart';
+import 'constants.dart';
 
 void main() {
   runApp(MyApp());
@@ -15,12 +17,13 @@ class MyApp extends StatelessWidget {
       title: 'Peer 2 Peer',
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        scaffoldBackgroundColor: kPrimaryColor,
       ),
       home: FutureBuilder(
         future: _initializeFirebase(),
         builder: (context, snapshot) {
           if(snapshot.connectionState == ConnectionState.done) {
-            return LoginScreen();
+            return DashboardScreen();
           }
           return Center(
             child: Scaffold(
--- lib/screens/dashboard/dashboard_screen.dart
+++ lib/screens/dashboard/dashboard_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../../constants.dart';
+
 class DashboardScreen extends StatefulWidget {
   const DashboardScreen({Key? key}) : super(key: key);
 
@@ -10,6 +12,61 @@ class DashboardScreen extends StatefulWidget {
 class _DashboardScreenState extends State<DashboardScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    Size size = MediaQuery.of(context).size;
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: kPrimaryColor,
+        elevation: 0.0,
+        leading: Center(
+          child: Text(
+            'Logo',
+            style: Theme.of(context)
+                .textTheme
+                .headline6!
+                .copyWith(color: kPrimaryTextColor),
+          ),
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.circle),
+          ),
+        ],
+        centerTitle: true,
+        title: Text('DASHBOARD'),
+      ),
+      body: Column(
+        children: [
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: kDefaultPadding),
+            margin: EdgeInsets.only(
+              left: kDefaultPadding,
+              right: kDefaultPadding,
+              top: size.height * .12
+            ),
+            child: TextFormField(
+              style: Theme.of(context)
+                  .textTheme
+                  .headline6!
+                  .copyWith(color: kPrimaryTextColor, fontSize: 17),
+              decoration: InputDecoration(
+                hintText: 'Create a new loan banner',
+                border: InputBorder.none,
+                hintStyle:
+                Theme.of(context).textTheme.headline6!.copyWith(
+                  color: kPrimaryTextColor.withOpacity(0.5),
+                  fontSize: 17
+                ),
+                suffixIcon: Icon(Icons.navigate_next, color: kPrimaryTextColor,)
+              ),
+            ),
+            decoration: BoxDecoration(
+              color: kPrimaryDarkColor,
+              borderRadius: BorderRadius.circular(25),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
