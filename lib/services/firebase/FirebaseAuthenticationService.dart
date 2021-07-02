8d42a537af8dc0153b43517edeb480ef45dedf92
simonojok19 <simonojok19@gmail.com>
2021-07-02 17:05:21 +0300

    finished the authentication service

--- lib/services/firebase/FirebaseAuthenticationService.dart
+++ lib/services/firebase/FirebaseAuthenticationService.dart
@@ -1,16 +1,22 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:peer2peer/services/authentication_api.dart';
 
 class FirebaseAuthenticationService implements AuthenticationService {
+  FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+
+  FirebaseAuthenticationService();
+
   @override
-  Future<String> createUserWithEmailAndPassword({required String email, required String password}) {
-    // TODO: implement createUserWithEmailAndPassword
-    throw UnimplementedError();
+  Future<String> createUserWithEmailAndPassword({required String email, required String password}) async {
+    final UserCredential userCredential = await _firebaseAuth
+        .createUserWithEmailAndPassword(email: email, password: password);
+    return userCredential.user!.uid;
   }
 
   @override
-  Future<String> signInWithEmailAndPassword({required String email, required String password}) {
-    // TODO: implement signInWithEmailAndPassword
-    throw UnimplementedError();
+  Future<String> signInWithEmailAndPassword({required String email, required String password}) async {
+    final UserCredential userCredential = await _firebaseAuth.signInWithEmailAndPassword(email: email, password: password);
+    return userCredential.user!.uid;
   }
 
 }
