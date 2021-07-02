71e23da7bfaecd8680be3a1a3682978ca8449f88
simonojok19 <simonojok19@gmail.com>
2021-07-02 17:41:44 +0300

    working on the bloc

--- lib/screens/login/login_bloc.dart
+++ lib/screens/login/login_bloc.dart
@@ -1,3 +1,23 @@
-class LoginBloc {
+import 'dart:async';
 
+import 'package:peer2peer/services/authentication_api.dart';
+
+class LoginBloc {
+  final AuthenticationService authenticationService;
+
+  final StreamController<String> _emailController = StreamController.broadcast();
+  Sink<String> get emailChanged => _emailController.sink;
+  Stream<String> get email => _emailController.stream;
+
+  final StreamController<String> _passwordController = StreamController.broadcast();
+  Sink<String> get passwordChanged => _passwordController.sink;
+  Stream<String> get password => _passwordController.stream;
+
+
+  LoginBloc({required this.authenticationService});
+
+  void dispose() {
+    _emailController.close();
+    _passwordController.close();
+  }
 }
