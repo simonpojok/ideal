29dc27672b1f74c9a0b4a76f38c4312d60c09399
simonojok19 <simonojok19@gmail.com>
2021-07-02 20:17:38 +0300

    added the sign in

--- lib/services/firebase/FirebaseAuthenticationService.dart
+++ lib/services/firebase/FirebaseAuthenticationService.dart
@@ -15,10 +15,17 @@ class FirebaseAuthenticationService implements AuthenticationService {
       final UserCredential userCredential = await _firebaseAuth
           .createUserWithEmailAndPassword(email: email, password: password);
       return AuthenticationMessage(
-          status: true, message: 'Registration Completed Successfully');
+        status: true,
+        message: 'Registration Completed Successfully',
+        user: PeerToPeerUser.User(
+          id: userCredential.user!.uid
+        )
+      );
     } on FirebaseAuthException catch (e) {
       final response = AuthenticationMessage(
-          status: false, message: 'Registration Completed Successfully');
+        status: false,
+        message: e.message ?? '',
+      );
       return response;
     }
   }
