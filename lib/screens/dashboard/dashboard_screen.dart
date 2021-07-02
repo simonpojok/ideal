847a472109476d9fcb734eb996a03aba13f650a0
simonojok19 <simonojok19@gmail.com>
2021-07-02 21:04:38 +0300

    migrated the colors

--- lib/screens/dashboard/dashboard_screen.dart
+++ lib/screens/dashboard/dashboard_screen.dart
@@ -37,35 +37,47 @@ class _DashboardScreenState extends State<DashboardScreen> {
       ),
       body: Column(
         children: [
-          Container(
-            padding: EdgeInsets.symmetric(horizontal: kDefaultPadding),
-            margin: EdgeInsets.only(
-              left: kDefaultPadding,
-              right: kDefaultPadding,
-              top: size.height * .12
-            ),
-            child: TextFormField(
-              style: Theme.of(context)
-                  .textTheme
-                  .headline6!
-                  .copyWith(color: kPrimaryTextColor, fontSize: 17),
-              decoration: InputDecoration(
-                hintText: 'Create a new loan banner',
-                border: InputBorder.none,
-                hintStyle:
-                Theme.of(context).textTheme.headline6!.copyWith(
-                  color: kPrimaryTextColor.withOpacity(0.5),
-                  fontSize: 17
-                ),
-                suffixIcon: Icon(Icons.navigate_next, color: kPrimaryTextColor,)
-              ),
-            ),
-            decoration: BoxDecoration(
-              color: kPrimaryDarkColor,
-              borderRadius: BorderRadius.circular(25),
-            ),
-          )
+          DashboardHeader()
         ],
+      ),
+    );
+  }
+}
+
+class DashboardHeader extends StatelessWidget {
+  const DashboardHeader({
+    Key? key,}) : super(key: key);
+
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: kDefaultPadding),
+      margin: EdgeInsets.only(
+        left: kDefaultPadding,
+        right: kDefaultPadding,
+        top: size.height * .12
+      ),
+      child: TextFormField(
+        style: Theme.of(context)
+            .textTheme
+            .headline6!
+            .copyWith(color: kPrimaryTextColor, fontSize: 17),
+        decoration: InputDecoration(
+          hintText: 'Create a new loan banner',
+          border: InputBorder.none,
+          hintStyle:
+          Theme.of(context).textTheme.headline6!.copyWith(
+            color: kPrimaryTextColor.withOpacity(0.5),
+            fontSize: 17
+          ),
+          suffixIcon: Icon(Icons.navigate_next, color: kPrimaryTextColor,)
+        ),
+      ),
+      decoration: BoxDecoration(
+        color: kPrimaryDarkColor,
+        borderRadius: BorderRadius.circular(25),
       ),
     );
   }
