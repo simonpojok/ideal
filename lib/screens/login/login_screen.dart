61c3d85446d34f18b8a9aadd37aaae8da8e67551
simonojok19 <simonojok19@gmail.com>
2021-07-02 20:39:06 +0300

    finished login page

--- lib/screens/login/login_screen.dart
+++ lib/screens/login/login_screen.dart
@@ -133,35 +133,77 @@ class _LoginScreenState extends State<LoginScreen> {
                     );
                   },
                 ),
+                Container(
+                  margin: EdgeInsets.only(top: kDefaultPadding * .02),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'Forgot password? ',
+                        style: Theme.of(context).textTheme.caption!.copyWith(
+                            fontSize: 16,
+                            color: kPrimaryTextColor.withOpacity(0.6)),
+                      ),
+                      TextButton(
+                        onPressed: () {},
+                        child: Text(
+                          'Reset',
+                          style: Theme.of(context).textTheme.caption!.copyWith(
+                                fontSize: 16,
+                                color: kPrimaryTextColor.withOpacity(0.6),
+                            decoration: TextDecoration.underline
+                              ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
                 StreamBuilder<bool>(
-                    stream: _loginBloc.enableLoginButton,
-                    builder: (context, snapshot) {
-                      return Container(
-                        margin: EdgeInsets.only(top: kDefaultPadding * 2),
-                        width: double.infinity,
-                        height: 50,
-                        child: ClipRRect(
-                          borderRadius: BorderRadius.circular(25),
-                          child: MaterialButton(
-                            onPressed: snapshot.data == true ? () {
-                              _loginBloc.loginEmailAndPassword();
-                            } : null,
-                            child: Center(
-                                child: Text(
-                              'Login',
-                              style: Theme.of(context)
-                                  .textTheme
-                                  .headline6!
-                                  .copyWith(color: kPrimaryTextColor),
-                            )),
-                          ),
+                  stream: _loginBloc.enableLoginButton,
+                  builder: (context, snapshot) {
+                    return Container(
+                      margin: EdgeInsets.only(top: kDefaultPadding * 2),
+                      width: double.infinity,
+                      height: 50,
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(25),
+                        child: MaterialButton(
+                          onPressed: snapshot.data == true
+                              ? () {
+                                  _loginBloc.loginEmailAndPassword();
+                                }
+                              : null,
+                          child: Center(
+                              child: Text(
+                            'Login',
+                            style: Theme.of(context)
+                                .textTheme
+                                .headline6!
+                                .copyWith(color: kPrimaryTextColor),
+                          )),
                         ),
-                        decoration: BoxDecoration(
-                          color: snapshot.data == true ? kPrimaryLightColor : kPrimaryLightColor.withOpacity(.4),
-                          borderRadius: BorderRadius.circular(25),
-                        ),
-                      );
-                    })
+                      ),
+                      decoration: BoxDecoration(
+                        color: snapshot.data == true
+                            ? kPrimaryLightColor
+                            : kPrimaryLightColor.withOpacity(.4),
+                        borderRadius: BorderRadius.circular(25),
+                      ),
+                    );
+                  },
+                ),
+                Container(
+                  margin: EdgeInsets.only(top: kDefaultPadding),
+                  child: TextButton(
+                    onPressed: () {},
+                    child: Text(
+                      'Create Account',
+                      style: Theme.of(context).textTheme.caption!.copyWith(
+                          fontSize: 16,
+                          color: kPrimaryTextColor.withOpacity(0.6)),
+                    ),
+                  ),
+                ),
               ],
             ),
           ),
