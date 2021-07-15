b4dd0a0a1ccc8ce59d4bb446e694a0f0b6509991
simonojok19 <simonojok19@gmail.com>
2021-07-15 23:44:52 +0300

    changed the login stuff

--- lib/screens/signup/signup_bloc.dart
+++ lib/screens/signup/signup_bloc.dart
@@ -1,42 +1,101 @@
 import 'dart:async';
 
-class SignUpBloc {
-  StreamController<String> _nameController = StreamController.broadcast();
-  Sink<String> get nameChanged => _nameController.sink;
-  Stream<String> get name => _nameController.stream;
+import 'package:flutter/material.dart';
+import 'package:peer2peer/services/authentication_api.dart';
+import 'package:peer2peer/services/models/authentication_message.dart';
 
-  StreamController<String> _emailController = StreamController.broadcast();
+class SignUpBloc {
+  final AuthenticationService authenticationService;
+
+  String _email = '';
+  bool _emailValid = false;
+
+  String _password = '';
+  bool _passwordValid = false;
+
+  String _confirmedPassword = '';
+  bool _confirmedPasswordValid = false;
+
+  final StreamController<String> _emailController =
+  StreamController.broadcast();
   Sink<String> get emailChanged => _emailController.sink;
   Stream<String> get email => _emailController.stream;
 
-  StreamController<String> _phoneNumberController = StreamController.broadcast();
-  Sink<String> get phoneNumberChanged => _phoneNumberController.sink;
-  Stream<String> get phoneNumber => _phoneNumberController.stream;
+  final StreamController<String> _passwordController =
+  StreamController.broadcast();
+  Sink<String> get passwordChanged => _passwordController.sink;
+  Stream<String> get password => _passwordController.stream;
 
-  StreamController<String> _nationalityController = StreamController.broadcast();
-  Sink<String> get nationalityChanged => _nationalityController.sink;
-  Stream<String> get nationality => _nationalityController.stream;
+  final StreamController<String> _passwordConfirmationController =
+  StreamController.broadcast();
+  Sink<String> get passwordConfirmationChanged => _passwordController.sink;
+  Stream<String> get passwordConfirmation => _passwordController.stream;
 
-  StreamController<String> _ninNumberController = StreamController.broadcast();
-  Sink<String> get ninNumberChanged => _ninNumberController.sink;
-  Stream<String> get ninNumber => _ninNumberController.stream;
+  final StreamController<bool> _enableLoginButtonController =
+  StreamController.broadcast();
+  Sink<bool> get enableLoginButtonChanged => _enableLoginButtonController.sink;
+  Stream<bool> get enableLoginButton => _enableLoginButtonController.stream;
 
-  StreamController<String> _nextKinController = StreamController.broadcast();
-  Sink<String> get nextKinChanged => _nextKinController.sink;
-  Stream<String> get nextKin => _nextKinController.stream;
+  SignUpBloc({required this.authenticationService}) {
+    email.listen((event) {
+      _email = event;
+      _emailValid = true;
+      _updateEnableLoginButtonStream();
+    }).onError((error) {
+      _email = "";
+      _emailValid = false;
+      _updateEnableLoginButtonStream();
+    });
 
-  StreamController<bool> _enableSignUpButtonController = StreamController.broadcast();
-  Sink<bool> get enableSignUpButtonChanged => _enableSignUpButtonController.sink;
-  Stream<bool> get enableSignUpButton => _enableSignUpButtonController.stream;
+    password.listen((event) {
+      _password = event;
+      _passwordValid = true;
+      _updateEnableLoginButtonStream();
+    }).onError((error) {
+      _password = '';
+      _passwordValid = false;
+      _updateEnableLoginButtonStream();
+    });
 
+    passwordConfirmation.listen((event) {
+      _confirmedPassword = event;
+      _confirmedPasswordValid = true;
+      _updateEnableLoginButtonStream();
+    }).onError((error) {
+      _confirmedPassword = '';
+      _confirmedPasswordValid = false;
+      _updateEnableLoginButtonStream();
+    });
+
+
+  }
+
+  void _updateEnableLoginButtonStream() {
+    if (_emailValid && _passwordValid) {
+      enableLoginButtonChanged.add(true);
+    } else {
+      enableLoginButtonChanged.add(false);
+    }
+  }
+
+  Future<String> loginEmailAndPassword() async {
+    String result = '';
+    if (_emailValid && _passwordValid) {
+      await authenticationService
+          .createUserWithEmailAndPassword(email: _email, password: _password)
+          .then((AuthenticationMessage message) {
+        result = 'Created user: $message';
+      });
+    } else {
+      return 'Login Password and Email is required';
+    }
+    return result;
+  }
 
   void dispose() {
-    _nameController.close();
     _emailController.close();
-    _phoneNumberController.close();
-    _nationalityController.close();
-    _ninNumberController.close();
-    _nextKinController.close();
-    _enableSignUpButtonController.close();
+    _passwordController.close();
+    _passwordConfirmationController.close();
+    _enableLoginButtonController.close();
   }
 }
--- lib/screens/signup/signup_screen.dart
+++ lib/screens/signup/signup_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:peer2peer/screens/signup/signup_bloc.dart';
 
 import '../../constants.dart';
 
@@ -10,6 +11,17 @@ class SignUpScreen extends StatefulWidget {
 }
 
 class _SignUpScreenState extends State<SignUpScreen> {
+  late SignUpBloc _signUpBloc;
+
+
+  @override
+  void initState() {
+    super.initState();
+    // _signUpBloc = SignUpBloc(
+    //
+    // );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,37 +36,42 @@ class _SignUpScreenState extends State<SignUpScreen> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Container(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Container(
-                    padding: EdgeInsets.symmetric(horizontal: 20),
-                    decoration: BoxDecoration(
-                      color: kPrimaryDarkColor,
-                      borderRadius: BorderRadius.circular(25)
-                    ),
-                    child: TextFormField(
-                      decoration: InputDecoration(
-                        hintText: 'Name',
-                        border: InputBorder.none,
-                        hintStyle: Theme.of(context).textTheme.headline6!.copyWith(
-                            color: kPrimaryTextColor.withOpacity(.3)
-                        )
+            StreamBuilder(
+              stream: null,
+              builder: (context, snapshot) {
+                return Container(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Container(
+                        padding: EdgeInsets.symmetric(horizontal: 20),
+                        decoration: BoxDecoration(
+                          color: kPrimaryDarkColor,
+                          borderRadius: BorderRadius.circular(25)
+                        ),
+                        child: TextFormField(
+                          decoration: InputDecoration(
+                            hintText: 'Name',
+                            border: InputBorder.none,
+                            hintStyle: Theme.of(context).textTheme.headline6!.copyWith(
+                                color: kPrimaryTextColor.withOpacity(.3)
+                            )
+                          ),
+                          style: Theme.of(context).textTheme.headline6!.copyWith(
+                            color: kPrimaryTextColor
+                          ),
+                        ),
                       ),
-                      style: Theme.of(context).textTheme.headline6!.copyWith(
-                        color: kPrimaryTextColor
-                      ),
-                    ),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 20, top: 5),
+                        child: Text('Required', style: Theme.of(context).textTheme.caption!.copyWith(
+                          color: Colors.red.shade300
+                        ),),
+                      )
+                    ],
                   ),
-                  Padding(
-                    padding: const EdgeInsets.only(left: 20, top: 5),
-                    child: Text('Required', style: Theme.of(context).textTheme.caption!.copyWith(
-                      color: Colors.red.shade300
-                    ),),
-                  )
-                ],
-              ),
+                );
+              }
             )
           ],
         ),
