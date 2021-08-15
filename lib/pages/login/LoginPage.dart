d3361fae094301ca4da9961b0d3916328db9bee7
simonojok19 <simonojok19@gmail.com>
2021-08-15 22:28:32 +0300

    handling the login form now in systematic way

--- lib/pages/login/LoginPage.dart
+++ lib/pages/login/LoginPage.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:ideal/pages/login/LoginBloc.dart';
+import 'package:ideal/services/AuthenticationService.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
@@ -8,8 +10,23 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  late LoginBloc _loginBloc;
+
+
+  @override
+  void initState() {
+    super.initState();
+    _loginBloc = LoginBloc(authenticationApi: AuthenticationService());
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold();
+  }
+
+  @override
+  void dispose() {
+    _loginBloc.dispose();
+    super.dispose();
   }
 }
