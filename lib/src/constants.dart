7c1b2105295795c39486c3a6ce26c91317bd2698
simonojok19 <simonojok19@gmail.com>
2021-08-20 21:44:26 +0300

    working with the text span

--- lib/src/constants.dart
+++ lib/src/constants.dart
@@ -4,5 +4,7 @@ const kPrimaryColor = Color(0xff2962ff);
 const kPrimaryLightColor = Color(0xff768fff);
 const kPrimaryDarkColor = Color(0xff0039cb);
 const kPrimaryTextColor = Color(0xffffffff);
+const kLightBlue = Color(0xff4C6BD1);
+const kColorOrange = Color(0xfff08721);
 
 const kDefaultPadding = 16.0;
--- lib/src/pages/lender/LenderPage.dart
+++ lib/src/pages/lender/LenderPage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:ideal/src/constants.dart';
 
 class LenderPage extends StatefulWidget {
   const LenderPage({Key? key}) : super(key: key);
@@ -11,7 +12,60 @@ class _LenderPageState extends State<LenderPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      body: SafeArea(
+        child: Container(
+          padding: EdgeInsets.symmetric(horizontal: kDefaultPadding),
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: kDefaultPadding),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text("LOGO"),
+                    Text(
+                      "REQUESTS",
+                      style: Theme.of(context)
+                          .textTheme
+                          .headline6!
+                          .copyWith(color: kLightBlue, fontSize: 18),
+                    ),
+                    Text(
+                      "Cancel",
+                      style: Theme.of(context)
+                          .textTheme
+                          .headline6!
+                          .copyWith(color: kColorOrange, fontSize: 18),
+                    ),
+                  ],
+                ),
+              ),
+              RichText(
+                  text: TextSpan(
+                    text: 'Please drag the pointer through the bar to indicate how much',
+                    children: [
+                      TextSpan(
+                        text: 'Money'
+                      ),
+                      TextSpan(
+                        text: ' you want to offer'
+                      ),
+                      TextSpan(
+                        text: ' Adam M.'
+                      )
+                    ]
+                  ),
+              ),
+              Row(
+                children: [
+                  Text("450,000 ugx"),
+                  Text("450,000 ugx"),
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
