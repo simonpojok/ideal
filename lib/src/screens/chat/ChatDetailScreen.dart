27521640067ffc33a34a6366780435da7bd0dae1
simonojok19 <simonojok19@gmail.com>
2021-08-28 23:29:15 +0300

    added the send message button

--- lib/src/screens/chat/ChatDetailScreen.dart
+++ lib/src/screens/chat/ChatDetailScreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:ideal/src/models/message.dart';
 
 class ChatDetailScreen extends StatefulWidget {
   const ChatDetailScreen({Key? key}) : super(key: key);
@@ -8,8 +9,92 @@ class ChatDetailScreen extends StatefulWidget {
 }
 
 class _ChatDetailScreenState extends State<ChatDetailScreen> {
+  final chat = chats[0];
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Color(0xFFF6F6F6),
+      appBar: AppBar(
+        brightness: Brightness.dark,
+        centerTitle: true,
+        title: RichText(
+          textAlign: TextAlign.center,
+          text: TextSpan(
+            text: chat.sender.name,
+            style: Theme.of(context).textTheme.headline6!.copyWith(
+                  fontSize: 16,
+                  fontWeight: FontWeight.w400,
+                  color: Colors.white,
+                ),
+            children: [
+              TextSpan(text: "\n"),
+              TextSpan(
+                text: 'Online',
+                style: Theme.of(context).textTheme.bodyText1!.copyWith(
+                      fontSize: 11,
+                      fontWeight: FontWeight.w400,
+                      color: Colors.greenAccent,
+                    ),
+              )
+            ],
+          ),
+        ),
+        leading: IconButton(
+          icon: Icon(
+            Icons.arrow_back_ios,
+            color: Colors.white,
+          ),
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.call,
+              color: Colors.white,
+            ),
+          ),
+        ],
+      ),
+      body: Column(
+        children: [
+          Container(
+            padding: EdgeInsets.symmetric(
+              horizontal: 10,
+              vertical: 5,
+            ),
+            decoration: BoxDecoration(
+              color: Colors.orangeAccent.withOpacity(.3),
+              borderRadius: BorderRadius.circular(50),
+            ),
+            child: Row(
+              children: [
+                IconButton(
+                  onPressed: () {},
+                  icon: Icon(Icons.file_copy_outlined),
+                  iconSize: 25,
+                  color: Theme.of(context).primaryColor,
+                ),
+                Expanded(
+                  child: TextField(
+                    decoration:
+                        InputDecoration.collapsed(hintText: "Send a message"),
+                    textCapitalization: TextCapitalization.sentences,
+                  ),
+                ),
+                IconButton(
+                  icon: Icon(Icons.send),
+                  iconSize: 25,
+                  color: Theme.of(context).primaryColor,
+                  onPressed: () {},
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
