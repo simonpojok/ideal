2483c78e68efcd17b7536f13fac7b6b472d5274b
simonojok19 <simonojok19@gmail.com>
2021-08-28 18:50:06 +0300

    created the payment history page

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:ideal/src/blocs/AuthenticationBloc.dart';
 import 'package:ideal/src/blocs/AuthenticationBlocProvider.dart';
-import 'package:ideal/src/screens/subscription/SubscriptionScreen.dart';
+import 'package:ideal/src/screens/payment_history/PaymentHistoryScreen.dart';
 import 'package:ideal/src/services/AuthenticationApi.dart';
 import 'package:ideal/src/services/AuthenticationService.dart';
 
@@ -32,9 +32,9 @@ class MyApp extends StatelessWidget {
               child: CircularProgressIndicator(),
             );
           } else if (snapshot.hasData && snapshot.data != "") {
-            return _buildMaterialApp(SubscriptionScreen());
+            return _buildMaterialApp(PaymentHistoryScreen());
           }
-          return _buildMaterialApp(SubscriptionScreen());
+          return _buildMaterialApp(PaymentHistoryScreen());
         },
       ),
     );
---
+++ lib/src/screens/payment_history/PaymentHistoryScreen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class PaymentHistoryScreen extends StatefulWidget {
+  const PaymentHistoryScreen({Key? key}) : super(key: key);
+
+  @override
+  _PaymentHistoryScreenState createState() => _PaymentHistoryScreenState();
+}
+
+class _PaymentHistoryScreenState extends State<PaymentHistoryScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Payment History"),
+      ),
+    );
+  }
+}
