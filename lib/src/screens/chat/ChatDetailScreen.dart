030e25c7d03c6cadeba3ebc788eb2114de2727e5
simonojok19 <simonojok19@gmail.com>
2021-08-28 21:45:05 +0300

    working on the chat details screen

---
+++ lib/src/screens/chat/ChatDetailScreen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ChatDetailScreen extends StatefulWidget {
+  const ChatDetailScreen({Key? key}) : super(key: key);
+
+  @override
+  _ChatDetailScreenState createState() => _ChatDetailScreenState();
+}
+
+class _ChatDetailScreenState extends State<ChatDetailScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
