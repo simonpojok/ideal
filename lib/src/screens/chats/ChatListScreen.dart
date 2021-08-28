6a600e076816313f031d0d741b08e759fedee9f2
simonojok19 <simonojok19@gmail.com>
2021-08-28 23:03:59 +0300

    finished the chat list for the user

--- lib/src/screens/chats/ChatListScreen.dart
+++ lib/src/screens/chats/ChatListScreen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:ideal/src/constants.dart';
+import 'package:ideal/src/models/message.dart';
 
 class ChatListScreen extends StatefulWidget {
   const ChatListScreen({Key? key}) : super(key: key);
@@ -11,9 +13,115 @@ class _ChatListScreenState extends State<ChatListScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
+        brightness: Brightness.dark,
+        elevation: 8,
         title: Text("Chats"),
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back_ios),
+          color: Colors.white,
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.search,
+              color: Colors.white,
+            ),
+          ),
+        ],
+      ),
+      body: ListView.separated(
+        itemBuilder: (BuildContext context, int index) {
+          final chat = chats[index];
+          return ListTile(
+            leading: Stack(
+              children: [
+                CircleAvatar(
+                  radius: 35,
+                  backgroundImage: AssetImage(chat.sender.imageUrl),
+                ),
+                if (chat.sender.isOnline)
+                  Positioned(
+                    right: 5,
+                    top: 5,
+                    child: Container(
+                      width: 15,
+                      height: 15,
+                      decoration: BoxDecoration(
+                        color: Colors.green,
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                    ),
+                  )
+              ],
+            ),
+            title: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  chat.sender.name,
+                  style: Theme.of(context).textTheme.headline4!.copyWith(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ),
+                ),
+                Text(
+                  chat.time,
+                  style: Theme.of(context).textTheme.headline6!.copyWith(
+                        fontSize: 11,
+                        fontWeight: FontWeight.w300,
+                        color: Colors.black54,
+                      ),
+                )
+              ],
+            ),
+            subtitle: Container(
+              margin: EdgeInsets.only(top: kDefaultPadding * 0.5),
+              child: Text(
+                chat.text,
+                style: Theme.of(context)
+                    .textTheme
+                    .bodyText1!
+                    .copyWith(fontSize: 13, color: Colors.black54),
+                overflow: TextOverflow.clip,
+                maxLines: 2,
+              ),
+            ),
+          );
+        },
+        separatorBuilder: (BuildContext context, int index) {
+          return Divider();
+        },
+        itemCount: chats.length,
       ),
     );
+  }
+}
+
+class UserOnlineIndicator extends StatelessWidget {
+  final bool isOnLine;
+  const UserOnlineIndicator({Key? key, required this.isOnLine})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return isOnLine
+        ? Container(
+            margin: const EdgeInsets.only(left: 5),
+            width: 7,
+            height: 7,
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              color: Theme.of(context).primaryColor,
+            ),
+          )
+        : Container(
+            child: null,
+          );
   }
 }
