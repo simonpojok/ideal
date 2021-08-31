ba9868d1610a97c6929bd27e000668190f9c6366
swaib <sswaib11@gmail.com>
2021-08-31 20:13:18 +0300

    added some color constants

--- lib/src/constants.dart
+++ lib/src/constants.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 
-const kPrimaryColor = Color(0xff2962ff);
-const kPrimaryLightColor = Color(0xff768fff);
-const kPrimaryDarkColor = Color(0xff0039cb);
+const kPrimaryColor = Color(0xff455a64);
+const kPrimaryLightColor = Color(0xff718792);
+const kPrimaryDarkColor = Color(0xff1c3131);
 const kPrimaryTextColor = Color(0xffffffff);
 const kLightBlue = Color(0xff4C6BD1);
 const kColorOrange = Color(0xfff08721);
