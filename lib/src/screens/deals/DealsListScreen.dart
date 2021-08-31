b751a699ea9b5967d9600684496e6a4a8028a41e
swaib <sswaib11@gmail.com>
2021-08-31 17:29:34 +0300

    working added the deals screen

---
+++ lib/src/screens/deals/DealsListScreen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class DealsListScreen extends StatefulWidget {
+  const DealsListScreen({Key? key}) : super(key: key);
+
+  @override
+  _DealsListScreenState createState() => _DealsListScreenState();
+}
+
+class _DealsListScreenState extends State<DealsListScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Deals"),
+      ),
+    );
+  }
+}
