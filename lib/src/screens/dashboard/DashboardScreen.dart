7e82e89507ec47f17d49c10a21ef5e9375e1f2c0
simonojok19 <simonojok19@gmail.com>
2021-08-31 11:36:30 +0300

    adding button navigation bar on the app

--- lib/src/screens/dashboard/DashboardScreen.dart
+++ lib/src/screens/dashboard/DashboardScreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:ideal/src/constants.dart';
 
 class DashboardScreen extends StatefulWidget {
   const DashboardScreen({Key? key}) : super(key: key);
@@ -10,6 +11,59 @@ class DashboardScreen extends StatefulWidget {
 class _DashboardScreenState extends State<DashboardScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Dashboard"),
+      ),
+      bottomNavigationBar: Container(
+        width: double.infinity,
+        height: 65,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            BottomNavigationButton(),
+            BottomNavigationButton(),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class BottomNavigationButton extends StatelessWidget {
+  const BottomNavigationButton({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Material(
+        child: InkWell(
+          onTap: () {},
+          child: Container(
+            color: Colors.white,
+            padding: EdgeInsets.symmetric(vertical: kDefaultPadding * .5),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Icon(
+                  Icons.feed,
+                  color: Colors.black54,
+                  size: 30,
+                ),
+                Text(
+                  "Feeds",
+                  style: Theme.of(context).textTheme.button!.copyWith(
+                        color: Colors.black54,
+                        fontSize: 15,
+                      ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
