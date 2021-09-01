207f9a6780ce22ae6aab3e48b9ff47995b179495
simonojok19 <simonojok19@gmail.com>
2021-09-01 18:47:53 +0300

    added total amount display widget

--- lib/src/screens/calc/loan_calculator.dart
+++ lib/src/screens/calc/loan_calculator.dart
@@ -16,9 +16,52 @@ class LoanCalculatorScreen extends StatefulWidget {
 class _LoanCalculatorScreenState extends State<LoanCalculatorScreen> {
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return Scaffold(
       appBar: AppBar(
         title: Text("Loan Calculator"),
+        elevation: 0,
+      ),
+      body: SafeArea(
+        child: Form(
+          child: Column(
+            children: [
+              Container(
+                width: size.width,
+                height: size.height * .3,
+                color: Theme.of(context).primaryColor,
+                child: Center(
+                  child: RichText(
+                    textAlign: TextAlign.center,
+                    text: TextSpan(
+                      text: "Total Amount\n500, 000",
+                      style: Theme.of(context)
+                          .primaryTextTheme
+                          .headline5!
+                          .copyWith(
+                            fontSize: 40,
+                            fontWeight: FontWeight.bold,
+                          ),
+                      children: [
+                        TextSpan(
+                          text: "\t\tUGX",
+                          style: Theme.of(context)
+                              .primaryTextTheme
+                              .headline6!
+                              .copyWith(
+                                fontSize: 20,
+                                fontWeight: FontWeight.normal,
+                                color: Colors.orangeAccent,
+                              ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
