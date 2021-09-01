216d668e59559243d334a35ff96ec7bb6fd901a9
simonojok19 <simonojok19@gmail.com>
2021-09-01 06:10:45 +0300

    refactored some codes

--- lib/src/screens/welcome/WelcomeScreen.dart
+++ lib/src/screens/welcome/WelcomeScreen.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:ideal/src/constants.dart';
 import 'package:ideal/src/screens/login/LoginScreen.dart';
+import 'package:ideal/src/screens/welcome/widgets/background.dart';
+import 'package:ideal/src/screens/widgets/buttons.dart';
 
 class WelcomeScreen extends StatefulWidget {
   const WelcomeScreen({Key? key}) : super(key: key);
@@ -57,93 +59,6 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
             ],
           ),
         ),
-      ),
-    );
-  }
-}
-
-class RoundedCornerButton extends StatelessWidget {
-  final String label;
-  final VoidCallback press;
-  final Color color;
-  const RoundedCornerButton({
-    Key? key,
-    required this.label,
-    required this.press,
-    required this.color,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    Size size = MediaQuery.of(context).size;
-    return ClipRRect(
-      borderRadius: BorderRadius.circular(20),
-      child: Container(
-        margin: EdgeInsets.symmetric(vertical: kDefaultPadding),
-        child: Material(
-          child: InkWell(
-            splashColor: Colors.greenAccent,
-            onTap: press,
-            child: Container(
-              width: size.width * .8,
-              padding: EdgeInsets.symmetric(
-                vertical: kDefaultPadding * .8,
-                horizontal: kDefaultPadding * 2,
-              ),
-              decoration: BoxDecoration(
-                color: color.withOpacity(.6),
-                borderRadius: BorderRadius.circular(20),
-              ),
-              child: Center(
-                child: Text(
-                  label,
-                  style: Theme.of(context).textTheme.button!.copyWith(
-                        color: Colors.white,
-                      ),
-                ),
-              ),
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class WelcomeScreenBackground extends StatelessWidget {
-  final Widget child;
-  const WelcomeScreenBackground({
-    Key? key,
-    required this.child,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    Size size = MediaQuery.of(context).size;
-    return Container(
-      height: size.height,
-      width: double.infinity,
-      child: Stack(
-        alignment: Alignment.center,
-        children: [
-          Positioned(
-            top: 0,
-            left: 0,
-            child: Image.asset(
-              "assets/images/main_top.png",
-              width: size.width * .3,
-            ),
-          ),
-          Positioned(
-            bottom: 0,
-            left: 0,
-            child: Image.asset(
-              "assets/images/main_bottom.png",
-              width: size.width * .3,
-            ),
-          ),
-          child,
-        ],
       ),
     );
   }
---
+++ lib/src/screens/widgets/buttons.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+import '../../constants.dart';
+
+class RoundedCornerButton extends StatelessWidget {
+  final String label;
+  final VoidCallback press;
+  final Color color;
+  const RoundedCornerButton({
+    Key? key,
+    required this.label,
+    required this.press,
+    required this.color,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(20),
+      child: Container(
+        margin: EdgeInsets.symmetric(vertical: kDefaultPadding),
+        child: Material(
+          child: InkWell(
+            splashColor: Colors.greenAccent,
+            onTap: press,
+            child: Container(
+              width: size.width * .8,
+              padding: EdgeInsets.symmetric(
+                vertical: kDefaultPadding * .8,
+                horizontal: kDefaultPadding * 2,
+              ),
+              decoration: BoxDecoration(
+                color: color.withOpacity(.6),
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: Center(
+                child: Text(
+                  label,
+                  style: Theme.of(context).textTheme.button!.copyWith(
+                        color: Colors.white,
+                      ),
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
