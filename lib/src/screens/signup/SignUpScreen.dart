166e31a01d696e9e39bfc09f52a233661bf4dbdd
simonojok19 <simonojok19@gmail.com>
2021-09-01 07:11:29 +0300

    finished the authentication screens

--- lib/src/screens/signup/SignUpScreen.dart
+++ lib/src/screens/signup/SignUpScreen.dart
@@ -34,8 +34,8 @@ class _SignUpScreenState extends State<SignUpScreen> {
                 ),
               ),
               SvgPicture.asset(
-                "assets/icons/login.svg",
-                width: size.height * .35,
+                "assets/icons/signup.svg",
+                width: size.height * .25,
               ),
               Expanded(child: Text("")),
               RoundCornerTextField(
@@ -66,10 +66,105 @@ class _SignUpScreenState extends State<SignUpScreen> {
                   Navigator.of(context)
                       .pushNamed(LoginScreen.LOGIN_SCREEN_ROUTE);
                 },
+              ),
+              OrDivider(),
+              Container(
+                margin: EdgeInsets.all(kDefaultPadding),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SocialMediaButton(
+                      imageSrc: "assets/icons/facebook.svg",
+                      onPress: () {},
+                    ),
+                    SocialMediaButton(
+                      imageSrc: "assets/icons/google-plus.svg",
+                      onPress: () {},
+                    ),
+                    SocialMediaButton(
+                      imageSrc: "assets/icons/twitter.svg",
+                      onPress: () {},
+                    ),
+                  ],
+                ),
               )
             ],
           ),
         ),
+      ),
+    );
+  }
+}
+
+class SocialMediaButton extends StatelessWidget {
+  final VoidCallback onPress;
+  final String imageSrc;
+  const SocialMediaButton({
+    Key? key,
+    required this.onPress,
+    required this.imageSrc,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onPress,
+      child: Container(
+        padding: EdgeInsets.all(kDefaultPadding),
+        margin: EdgeInsets.symmetric(
+          horizontal: kDefaultPadding,
+        ),
+        decoration: BoxDecoration(
+          border: Border.all(
+            color: Theme.of(context).primaryColorLight,
+            width: 2,
+          ),
+          shape: BoxShape.circle,
+        ),
+        child: SvgPicture.asset(
+          imageSrc,
+          width: kDefaultPadding,
+          height: kDefaultPadding,
+        ),
+      ),
+    );
+  }
+}
+
+class OrDivider extends StatelessWidget {
+  const OrDivider({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Container(
+      width: size.width * .8,
+      child: Row(
+        children: [
+          Expanded(
+            child: Divider(
+              color: Theme.of(context).primaryColor,
+              height: 1.5,
+            ),
+          ),
+          Padding(
+            padding:
+                const EdgeInsets.symmetric(horizontal: kDefaultPadding * .5),
+            child: Text(
+              "OR",
+              style: TextStyle(
+                color: Theme.of(context).primaryColor,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+          ),
+          Expanded(
+            child: Divider(
+              color: Theme.of(context).primaryColor,
+              height: 1.5,
+            ),
+          ),
+        ],
       ),
     );
   }
--- lib/src/screens/signup/widgets/background.dart
+++ lib/src/screens/signup/widgets/background.dart
@@ -21,15 +21,15 @@ class SignUpScreenBackground extends StatelessWidget {
             top: 0,
             left: 0,
             child: Image.asset(
-              "assets/images/main_top.png",
+              "assets/images/signup_top.png",
               width: size.width * .3,
             ),
           ),
           Positioned(
             bottom: 0,
-            right: 0,
+            left: 0,
             child: Image.asset(
-              "assets/images/login_bottom.png",
+              "assets/images/main_bottom.png",
               width: size.width * .3,
             ),
           ),
