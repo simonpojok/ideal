71f65a88d700b085fe651b9e1ef979e1342c3456
simonojok19 <simonojok19@gmail.com>
2021-09-01 19:42:51 +0300

    working on the load calculations

--- lib/src/screens/calc/loan_calculator.dart
+++ lib/src/screens/calc/loan_calculator.dart
@@ -17,6 +17,19 @@ class LoanCalculatorScreen extends StatefulWidget {
 
 class _LoanCalculatorScreenState extends State<LoanCalculatorScreen> {
   final _formKey = GlobalKey<FormState>();
+  String duration = 'Day';
+  String percentage = "%";
+
+  void _calculateLoan() {
+    // double interestRate = (double.parse(_interestRate.text) / 100) / 12;
+    // int months = int.parse(_period.text) * 12;
+    // double monthlyTop = interestRate * pow((1 + interestRate), months);
+    // double monthlyBottom = pow(1 + interestRate, months) - 1;
+    // double monthlyRate =
+    //     double.parse(_mortgageAmount.text) * (monthlyTop / monthlyBottom);
+    // double totalPayment = monthlyRate * months;
+  }
+
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
@@ -36,44 +49,161 @@ class _LoanCalculatorScreenState extends State<LoanCalculatorScreen> {
         ],
       ),
       body: SafeArea(
-        child: Form(
-          key: _formKey,
-          child: Column(
-            children: [
-              HeaderDisplay(),
-              Container(
-                margin: EdgeInsets.symmetric(vertical: kDefaultPadding),
-                width: size.width * .9,
-                child: TextFormField(
-                  decoration: InputDecoration(
-                    filled: true,
-                    border: InputBorder.none,
-                    hintText: "Enter Amount",
-                    hintStyle: Theme.of(context).textTheme.bodyText1!.copyWith(
-                          color: Colors.black45,
-                          fontSize: 16,
-                        ),
-                    prefixIcon: Icon(
-                      Icons.monetization_on_outlined,
-                      color: Colors.black45,
-                    ),
-                    suffix: Text("UGX"),
+        child: SingleChildScrollView(
+          child: Form(
+            key: _formKey,
+            child: Column(
+              children: [
+                HeaderDisplay(),
+                Container(
+                  margin: EdgeInsets.symmetric(vertical: kDefaultPadding),
+                  width: size.width * .9,
+                  child: CalculatorEditText(
+                    hint: "Amount",
+                    leadingIcon: Icons.monetization_on_outlined,
                   ),
-                  style: Theme.of(context).textTheme.bodyText1!.copyWith(
-                        color: Colors.black54,
-                        fontSize: 16,
-                      ),
                 ),
-              ),
-              RoundedCornerButton(
-                label: "Calculate",
-                press: () {},
-                color: Theme.of(context).primaryColor,
-              ),
-            ],
+                Container(
+                  margin: EdgeInsets.symmetric(vertical: kDefaultPadding),
+                  width: size.width * .9,
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: CalculatorEditText(
+                          leadingIcon: Icons.calendar_today,
+                          hint: 'Duration',
+                        ),
+                      ),
+                      Expanded(
+                        child: SelectDropDown(
+                          value: duration,
+                          onValueChange: (String? value) {
+                            setState(() {
+                              duration = value!;
+                            });
+                          },
+                          options: ["Day", "Week", "Month", "Year"],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.symmetric(vertical: kDefaultPadding),
+                  width: size.width * .9,
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: CalculatorEditText(
+                          leadingIcon: Icons.rate_review,
+                          hint: 'Rate',
+                        ),
+                      ),
+                      Expanded(
+                        child: SelectDropDown(
+                          value: percentage,
+                          onValueChange: (String? value) {
+                            setState(() {
+                              percentage = value!;
+                            });
+                          },
+                          options: ["%"],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                RoundedCornerButton(
+                  label: "Calculate",
+                  press: () {
+                    _calculateLoan();
+                  },
+                  color: Theme.of(context).primaryColor,
+                ),
+              ],
+            ),
           ),
         ),
       ),
+    );
+  }
+}
+
+class SelectDropDown extends StatelessWidget {
+  final String value;
+  final Function(String? value) onValueChange;
+  final List<String> options;
+  const SelectDropDown(
+      {Key? key,
+      required this.value,
+      required this.onValueChange,
+      required this.options})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(left: kDefaultPadding * .5),
+      padding: EdgeInsets.symmetric(horizontal: kDefaultPadding),
+      color: Colors.black12,
+      child: Center(
+        child: DropdownButton<String>(
+          value: value,
+          iconSize: 24,
+          elevation: 16,
+          underline: Container(),
+          onChanged: onValueChange,
+          items: options.map<DropdownMenuItem<String>>((String value) {
+            return DropdownMenuItem<String>(
+              value: value,
+              child: Text(
+                value,
+                style: Theme.of(context).textTheme.bodyText1!.copyWith(
+                      color: Colors.black54,
+                      fontSize: 16,
+                    ),
+              ),
+            );
+          }).toList(),
+        ),
+      ),
+    );
+  }
+}
+
+class CalculatorEditText extends StatelessWidget {
+  final String hint;
+  final IconData leadingIcon;
+  final String? tailingText;
+  const CalculatorEditText(
+      {Key? key,
+      required this.hint,
+      required this.leadingIcon,
+      this.tailingText})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      decoration: InputDecoration(
+        filled: true,
+        border: InputBorder.none,
+        hintText: hint,
+        hintStyle: Theme.of(context).textTheme.bodyText1!.copyWith(
+              color: Colors.black45,
+              fontSize: 16,
+            ),
+        prefixIcon: Icon(
+          leadingIcon,
+          color: Theme.of(context).primaryColorLight,
+        ),
+        suffixText: tailingText,
+      ),
+      style: Theme.of(context).textTheme.bodyText1!.copyWith(
+            color: Colors.black54,
+            fontSize: 16,
+          ),
+      keyboardType: TextInputType.numberWithOptions(decimal: true),
     );
   }
 }
