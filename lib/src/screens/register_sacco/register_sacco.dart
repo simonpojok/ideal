fc7057ddb58e83c02f91c5a8d9201f7d86ad8930
simonojok19 <simonojok19@gmail.com>
2021-09-07 09:41:09 +0300

    created the register sacco screen

---
+++ lib/src/screens/register_sacco/register_sacco.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class RegisterSaccoScreen extends StatefulWidget {
+  static const REGISTER_SACCO_SCREEN = "/register_sacco";
+
+  const RegisterSaccoScreen({Key? key}) : super(key: key);
+
+  @override
+  _RegisterSaccoScreenState createState() => _RegisterSaccoScreenState();
+  static void goToRegisterSaccoScreen(BuildContext context) {
+    Navigator.of(context).pushNamed(REGISTER_SACCO_SCREEN);
+  }
+}
+
+class _RegisterSaccoScreenState extends State<RegisterSaccoScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
