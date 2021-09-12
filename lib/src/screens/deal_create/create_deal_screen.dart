8122ebe9bf4b6d9e07cbb0f1fd017b351a5deb23
simonojok19 <simonojok19@gmail.com>
2021-09-12 16:50:16 +0300

    made the wiget reusable

--- lib/src/screens/deal_create/create_deal_screen.dart
+++ lib/src/screens/deal_create/create_deal_screen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:ideal/src/screens/widgets/filled_text_input.dart';
+import 'package:ideal/src/screens/widgets/loan_duration_picker.dart';
+
+import '../../constants.dart';
 
 class CreateDealScreen extends StatefulWidget {
   static const CREATE_DEAL_SCREEN_ROUTE = "/create_deal_screen_route";
@@ -14,6 +18,9 @@ class CreateDealScreen extends StatefulWidget {
 }
 
 class _CreateDealScreenState extends State<CreateDealScreen> {
+  final _amountController = TextEditingController();
+  final _durationController = TextEditingController();
+  String selectedDuration = "Daily";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,13 +29,102 @@ class _CreateDealScreenState extends State<CreateDealScreen> {
       ),
       body: SafeArea(
         child: Container(
+          padding: EdgeInsets.only(
+            top: kDefaultPadding,
+            left: kDefaultPadding,
+            right: kDefaultPadding,
+          ),
           child: SingleChildScrollView(
             child: Column(
-              children: [],
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Container(
+                  padding: EdgeInsets.symmetric(vertical: kDefaultPadding),
+                  child: Text(
+                    "Create a Loan Request",
+                    style: Theme.of(context).textTheme.headline5!.copyWith(
+                          color: Colors.black54,
+                          fontSize: 20,
+                          fontWeight: FontWeight.normal,
+                        ),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                TextInputLabeled(
+                  label: "Expected Amount",
+                  hint: "Amount ( UGX )",
+                  amountController: _amountController,
+                  lineNumber: 1,
+                  keyboard: TextInputType.numberWithOptions(decimal: true),
+                ),
+                Container(
+                  margin: EdgeInsets.only(top: kDefaultPadding * .5),
+                  child: Text(
+                    "Frequency",
+                    style: Theme.of(context).textTheme.bodyText1!.copyWith(
+                          fontSize: 15,
+                          color: Colors.black45,
+                          fontWeight: FontWeight.normal,
+                        ),
+                  ),
+                ),
+                LoanDurationPicker(
+                  durationController: _durationController,
+                  options: ["Daily", "Weekly", "Monthly", "Yearly"],
+                  onValueChanged: (String? value) {
+                    setState(() {
+                      selectedDuration = value ?? selectedDuration;
+                    });
+                  },
+                  selectedValue: selectedDuration,
+                )
+              ],
             ),
           ),
         ),
       ),
+    );
+  }
+}
+
+class TextInputLabeled extends StatelessWidget {
+  final TextEditingController _amountController;
+  final String label;
+  final String hint;
+  final int lineNumber;
+  final TextInputType keyboard;
+  const TextInputLabeled({
+    Key? key,
+    required TextEditingController amountController,
+    required this.label,
+    required this.hint,
+    required this.lineNumber,
+    required this.keyboard,
+  })  : _amountController = amountController,
+        super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Text(
+          label,
+          style: Theme.of(context).textTheme.bodyText1!.copyWith(
+                fontSize: 15,
+                color: Colors.black45,
+                fontWeight: FontWeight.normal,
+              ),
+        ),
+        SizedBox(height: 10),
+        FilledTextInput(
+          onChange: (String value) {},
+          hint: hint,
+          keyboard: keyboard,
+          maxLine: lineNumber,
+          controller: _amountController,
+        ),
+      ],
     );
   }
 }
