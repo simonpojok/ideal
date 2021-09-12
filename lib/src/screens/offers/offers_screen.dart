021173ed4a76394f1bc3f654d732fea70ddc1e74
simonojok19 <simonojok19@gmail.com>
2021-09-12 14:56:21 +0300

    fixed the circular progroess not in the center

--- lib/src/screens/offers/offers_screen.dart
+++ lib/src/screens/offers/offers_screen.dart
@@ -60,26 +60,30 @@ class _OffersScreenState extends State<OffersScreen> {
                 );
               }
 
-              return Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  SizedBox(
-                    width: 60,
-                    height: 60,
-                    child: CircularProgressIndicator(
-                      strokeWidth: 6,
+              return Container(
+                width: double.infinity,
+                height: double.infinity,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    SizedBox(
+                      width: 60,
+                      height: 60,
+                      child: CircularProgressIndicator(
+                        strokeWidth: 6,
+                      ),
                     ),
-                  ),
-                  Text(
-                    "Loading...",
-                    style: Theme.of(context).textTheme.headline5!.copyWith(
-                          fontWeight: FontWeight.normal,
-                          fontSize: 16,
-                          color: Colors.black54,
-                        ),
-                  )
-                ],
+                    Text(
+                      "Loading...",
+                      style: Theme.of(context).textTheme.headline5!.copyWith(
+                            fontWeight: FontWeight.normal,
+                            fontSize: 16,
+                            color: Colors.black54,
+                          ),
+                    )
+                  ],
+                ),
               );
             }),
       ),
