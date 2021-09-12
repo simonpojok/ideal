07fcf3101493aa912288bbe33303f8a8d0fc7688
simonojok19 <simonojok19@gmail.com>
2021-09-12 11:28:11 +0300

    added the price picker labels

--- lib/src/screens/offer/OfferScreen.dart
+++ lib/src/screens/offer/OfferScreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:ideal/src/constants.dart';
 
 class OfferScreen extends StatefulWidget {
   static const OFFER_SCREEN_ROUTE = '/offer_screen';
@@ -15,6 +16,109 @@ class OfferScreen extends StatefulWidget {
 class _OfferScreenState extends State<OfferScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Publish Load Offer"),
+      ),
+      body: SafeArea(
+        child: Container(
+          padding: EdgeInsets.only(
+            left: kDefaultPadding * .5,
+            right: kDefaultPadding * .5,
+            top: kDefaultPadding * .5,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(
+                  bottom: kDefaultPadding * .5,
+                ),
+                child: Text(
+                  "Loan Range",
+                  style: Theme.of(context).textTheme.bodyText1!.copyWith(
+                        fontSize: 18,
+                        color: Colors.black45,
+                      ),
+                ),
+              ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  RangePriceInput(),
+                  Container(
+                    padding: EdgeInsets.symmetric(
+                      horizontal: kDefaultPadding * .5,
+                    ),
+                    child: Center(
+                      child: Text(
+                        "To",
+                        style: Theme.of(context).textTheme.bodyText1!.copyWith(
+                              fontSize: 18,
+                              color: Colors.black45,
+                            ),
+                      ),
+                    ),
+                  ),
+                  RangePriceInput(),
+                ],
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              TextFormField(
+                decoration: InputDecoration(
+                  filled: true,
+                  border: InputBorder.none,
+                  fillColor: Colors.black12,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class RangePriceInput extends StatelessWidget {
+  const RangePriceInput({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+        child: Container(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(
+              bottom: kDefaultPadding * .5,
+            ),
+            child: Text(
+              "Loan Range",
+              style: Theme.of(context).textTheme.bodyText1!.copyWith(
+                    fontSize: 15,
+                    color: Colors.black45,
+                    fontWeight: FontWeight.normal,
+                  ),
+            ),
+          ),
+          TextFormField(
+            decoration: InputDecoration(
+              filled: true,
+              border: InputBorder.none,
+              fillColor: Colors.black12,
+            ),
+            style: Theme.of(context).textTheme.bodyText1!.copyWith(
+                  fontSize: 18,
+                  color: Colors.black45,
+                ),
+          ),
+        ],
+      ),
+    ));
   }
 }
