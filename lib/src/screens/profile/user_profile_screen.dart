d00a55df3c503f842346c7bafd34d1158f404208
simonojok19 <simonojok19@gmail.com>
2021-10-10 19:14:23 +0300

    fixing some loading errors

--- lib/src/screens/profile/user_profile_screen.dart
+++ lib/src/screens/profile/user_profile_screen.dart
@@ -5,6 +5,7 @@ import 'package:ideal/src/screens/profile/widgets/decorated_text_field.dart';
 import 'package:ideal/src/screens/profile/widgets/profile_header.dart';
 import 'package:ideal/src/screens/widgets/buttons.dart';
 import 'package:ideal/src/screens/widgets/labeled_text_input.dart';
+import 'package:ideal/src/services/share_preferences/shared_preference_data.dart';
 
 import '../../constants.dart';
 
@@ -28,6 +29,13 @@ class _UserProfileScreenState extends State<UserProfileScreen> {
   final _nationalIdNumberController = TextEditingController();
   final _districtController = TextEditingController();
   final _villageController = TextEditingController();
+  LocalUser? _localUser;
+
+  @override
+  void initState() {
+    super.initState();
+    SharedPreferenceData.getCurrentUser().then((value) => _localUser = value);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -41,67 +49,95 @@ class _UserProfileScreenState extends State<UserProfileScreen> {
       body: SafeArea(
         child: SingleChildScrollView(
           child: FutureBuilder(
-            future: userProfileApi.getCurrentUser(),
-            builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
-              bool hasError = snapshot.hasError;
-              bool hasData = snapshot.hasData && !snapshot.data!.exists;
-              bool loading = !(snapshot.connectionState == ConnectionState.done);
-              // Map<String, dynamic>? data = hasData ? snapshot.data!.data() as Map<String, dynamic> : null;
-              Map<String, dynamic>? data = (hasData && !loading) ? snapshot.data.data() : null;
+              future: userProfileApi.getCurrentUser(),
+              builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
+                bool hasError = snapshot.hasError;
+                bool hasData = snapshot.hasData && !snapshot.data!.exists;
+                bool loading =
+                    !(snapshot.connectionState == ConnectionState.done);
+                // Map<String, dynamic>? data = hasData ? snapshot.data!.data() as Map<String, dynamic> : null;
+                Map<String, dynamic>? data =
+                    (hasData && !loading) ? snapshot.data.data() : null;
 
-              if (data != null) {
-                LocalUser user = LocalUser.fromJson(data);
-                _firstNameController.text = user.firstName;
-                _lastNameController.text = user.lastName;
-                _emailController.text = user.email;
-                _nationalIdNumberController.text = user.nin;
-                _districtController.text = user.district;
-                _villageController.text = user.village;
-              }
+                if (data != null) {
+                  LocalUser user = LocalUser.fromJson(data);
+                  _firstNameController.text = user.firstName;
+                  _lastNameController.text = user.lastName;
+                  _emailController.text = user.email;
+                  _nationalIdNumberController.text = user.nin;
+                  _districtController.text = user.district;
+                  _villageController.text = user.village;
+                }
+                _emailController.text = _localUser?.email ?? "";
 
-
-              print(data);
-              return Container(
-                child: Column(
-                  children: [
-                    UserProfileHeader(),
-                    DecoratedTextField(
-                      controller: _firstNameController,
-                      label: "First Name",
-                    ),
-                    DecoratedTextField(
-                      controller: _lastNameController,
-                      label: "Last Name",
-                    ),
-                    DecoratedTextField(
-                      enabled: false,
-                      controller: _emailController,
-                      label: "Email",
-                    ),
-                    DecoratedTextField(
-                      controller: _nationalIdNumberController,
-                      label: "NIN",
-                    ),
-                    DecoratedTextField(
-                      controller: _districtController,
-                      label: "District",
-                    ),
-                    DecoratedTextField(
-                      controller: _villageController,
-                      label: "Village",
-                    ),
-                    RoundedCornerButton(
-                      color: kPrimaryDarkColor,
-                      press: () {},
-                      label: 'Update',
-                    ),
-                  ],
-                ),
-              );
-            }
-          ),
+                return Container(
+                  child: Column(
+                    children: [
+                      UserProfileHeader(),
+                      DecoratedTextField(
+                        controller: _firstNameController,
+                        label: "First Name",
+                      ),
+                      DecoratedTextField(
+                        controller: _lastNameController,
+                        label: "Last Name",
+                      ),
+                      DecoratedTextField(
+                        enabled: false,
+                        controller: _emailController,
+                        label: "Email",
+                      ),
+                      DecoratedTextField(
+                        controller: _nationalIdNumberController,
+                        label: "NIN",
+                      ),
+                      DecoratedTextField(
+                        controller: _districtController,
+                        label: "District",
+                      ),
+                      DecoratedTextField(
+                        controller: _villageController,
+                        label: "Village",
+                      ),
+                      RoundedCornerButton(
+                        color: kPrimaryDarkColor,
+                        press: () async {
+                          final user = LocalUser(
+                            id: _localUser?.id ?? "",
+                            username: "",
+                            firstName: _firstNameController.text,
+                            lastName: _lastNameController.text,
+                            email: _localUser?.email ?? "",
+                            nin: _nationalIdNumberController.text,
+                            district: _districtController.text,
+                            village: _villageController.text,
+                          );
+                          userProfileApi
+                              .createOrUpdateCurrentUser(user)
+                              .then((value) => print("User Created"))
+                              .catchError((error) => print("Error occurred"));
+                        },
+                        label: 'Update',
+                      ),
+                    ],
+                  ),
+                );
+              }),
         ),
       ),
     );
+  }
+
+  void createLocalUserProfile() {
+    // LocalUser({
+    //   required this.id,
+    //   required this.username,
+    //   required this.firstName,
+    //   required this.lastName,
+    //   required this.email,
+    //   required this.nin,
+    //   required this.district,
+    //   required this.village,
+    // });
   }
 }
